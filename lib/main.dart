5020ceb27565b0e1432dc9b5a0648c5ed12cf56c
sama <smatarek25@gmail.com>
2025-07-25 16:04:45 +0300

    * integrate android with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:evently_application/core/constants/app_theme_style.dart';
 import 'package:evently_application/modules/home%20screen/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
