cb969da172644a823dcd227892f708d4f05d3a81
sama <smatarek25@gmail.com>
2025-07-28 13:56:52 +0300

    * implement google sign in logic

---
+++ lib/firebase_service.dart
@@ -0,0 +1,5 @@
+import 'package:evently_application/models/event_model.dart';
+
+class FirebaseService {
+  static void createEvent(EventModel event) {}
+}
--- lib/modules/authentication/login_screen.dart
+++ lib/modules/authentication/login_screen.dart
@@ -2,9 +2,14 @@ import 'package:evently_application/core/constants/app_assets.dart';
 import 'package:evently_application/core/constants/app_colors.dart';
 import 'package:evently_application/modules/authentication/forget_password_screen.dart';
 import 'package:evently_application/modules/authentication/register_screen.dart';
+import 'package:evently_application/modules/home%20screen/home_screen.dart';
 import 'package:evently_application/widgets/elevated_button.dart';
 import 'package:evently_application/widgets/text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -19,6 +24,23 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  Future signInWithGoogle(BuildContext context) async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) return null;
+
+    final GoogleSignInAuthentication? googleAuth = await googleUser
+        ?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => HomeScreen()));
+    // Navigator.push(context, MaterialPageRoute(builder: (context)=>ForgetPasswordScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +127,66 @@ class _LoginScreenState extends State<LoginScreen> {
                       )
                   )
                 ],
-              )
+              ),
+              SizedBox(height: 24,),
+              Row(
+                children: [
+                  Expanded(
+                      child: Divider(
+                        color: AppColors.primaryColor,
+                        indent: 15,
+                        endIndent: 10,
+                        thickness: 1.25,
+                      )
+                  ),
+                  Text("or",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Inter',
+                    ),
+                  ),
+                  Expanded(
+                      child: Divider(
+                        color: AppColors.primaryColor,
+                        indent: 15,
+                        endIndent: 10,
+                        thickness: 1.25,
+                      )
+                  ),
+                ],
+              ),
+              SizedBox(height: 24,),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.backgroundLight,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 74, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          side: BorderSide(
+                            color: AppColors.primaryColor,
+                            width: 1,
+                          )
+                      )
+                  ),
+                  onPressed: () => signInWithGoogle(context),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset("assets/icons/google.svg"),
+                      SizedBox(width: 10,),
+                      Text("Login With Google",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            color: AppColors.primaryColor,
+                          )
+                      )
+                    ],
+                  )
+              ),
             ],
           ),
         ),
@@ -113,3 +194,4 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
+
