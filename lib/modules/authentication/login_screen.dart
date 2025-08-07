48ff8051a90be53925326d3868b34a5c4dfb2245
smata <smatarek25@gmail.com>
2025-08-07 14:06:09 +0300

    * add error messages in auth

--- lib/modules/authentication/login_screen.dart
+++ lib/modules/authentication/login_screen.dart
@@ -48,6 +48,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.backgroundLight,
       body: SafeArea(
         child: Padding(
--- lib/modules/authentication/register_screen.dart
+++ lib/modules/authentication/register_screen.dart
@@ -29,6 +29,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.backgroundLight,
       body: SafeArea(
         child: Padding(
